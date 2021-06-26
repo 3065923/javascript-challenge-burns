b6f80ba91793df29f8fb1d34eae0f1212e93e9c4
3065923 <burnsjake5253@gmail.com>
2021-06-26 02:22:45 -0400

    Table button/form creation works. Need to fix filter to only show filtered table data.

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -37,3 +37,71 @@ tableData.forEach((alienSightings) => {
       rowData.text(value);
     });
   });
+
+
+
+// // testing
+// function filteredTableFunction(sightings) {
+//     return sightings.datetime ==inputValue;
+// }
+
+// var testTableFilteredMaybe= tableData.filter(filteredTableFunction);
+// console.log(testTableFilteredMaybe)
+
+
+
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select("#filter-form");
+
+// Create event handlers for clicking the button or pressing the enter key
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Create the function to run for both events
+function runEnter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  // Print the value to the console
+  console.log(inputValue);
+  console.log(tableData);
+
+  var filteredData = tableData.filter(sightings => sightings.datetime === inputValue);
+
+  console.log(filteredData);
+
+//   select all rows that exist in the table so you may filter
+  var filterRow = d3.select("tr");
+
+
+//   testing filter table
+  function filteredTableFunction(sightings) {
+    return sightings.datetime ==inputValue;
+  }
+
+
+  var tableFiltered= tableData.filter(filteredTableFunction);
+  console.log(tableFiltered);
+
+  tableFiltered.forEach((alienSightings) => {
+    var row = tbody.append("tr");
+    Object.entries(alienSightings).forEach(([key, value]) => {
+      var rowData = row.append("td");
+      rowData.text(value);
+    });
+  });
+
+
+
+}
